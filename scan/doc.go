824553edbc41db52a99738d4f4166e6038f25896
GoLangsam <GoLangsam.APA@a-bc.net>
2018-12-10 03:02:56 +0100

    new: scan/

---
+++ scan/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2018 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/* Package scanner provides some dead-simple useful scanners starting with
+   ScanLinesOfWords
+*/
+package scan
---
+++ scan/scanner.go
@@ -0,0 +1,48 @@
+// Copyright 2018 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scan
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"strings"
+)
+
+func linesOfWords(out chan<- []string, r io.Reader, skipEmptyLines bool) {
+	defer close(out)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+
+		scanner := bufio.NewScanner(strings.NewReader(scanner.Text()))
+		scanner.Split(bufio.ScanWords)
+
+		words := []string{}
+		for scanner.Scan() {
+			words = append(words, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal("Word-Scanner encountered:", err)
+		}
+
+		if skipEmptyLines && len(words) < 1 {
+			continue
+		}
+
+		out <- words
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Line-Scanner encountered:", err)
+	}
+	return
+}
+
+// LinesOfWords returns a channel to receive the slice of words as found in each line.
+func LinesOfWords(r io.Reader, skipEmptyLines bool) <-chan []string {
+	cha := make(chan []string)
+	go linesOfWords(cha, r, skipEmptyLines)
+	return cha
+}
