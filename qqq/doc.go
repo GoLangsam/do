990daf4771787b32d2ed4d85239a0cdbd702ff97
GoLangsam <GoLangsam.APA@a-bc.net>
2019-07-10 14:59:31 +0630

    qqq: add tab & eol; remove the xx_ flavor - no need: std logger adds eol by himself

--- qqq/doc.go
+++ qqq/doc.go
@@ -4,28 +4,31 @@
 
 // Package qqq provides easy printing, logging and panicing to any package.
 //
-// It is intentionally minimalistic - part of my "poor mans survival kit".
+// It is intentionally minimalistic - part of a "poor mans survival kit".
 //
 // `qqq.log.go` provides easy-to-use and easy-to-remember functions:
 //  qqq - print to log iff verbose
 //  see - print to log
 //  die - panic to log
 //
-// Each function comes in two more flavours:
-//  xx_ - inline-print, no line-feed
+// Each function comes in one more flavour:
 //  xxf - formated print
 //
 // `qqq.set.go` exports functions to control the package-wide verbosity.
-// Useful where other packages need to control verbosity applicationwide.
+// This allows other packages to control the verbosity of imported packages.
 //
 // `qqq.main.init`.go has a super-simple init() function overriding defaults
-// of the standard package. Most useful in /cmd main() packages!
+// of the standard package. Use in /cmd main() packages only!
 //
 // Note: Intentionally, log.Fatal is not included.
 // IMO its use in packages is bad practice.
 //
 // The idea and the use of qqq as name were inspired by a talk I once listened to.
 // TODO: Find and supply reference to the original author.
+//
+// Last, but not least: the ever-useful constants
+//  `tab` = "\t" for TABulation and
+//  `eol` = "\n" for End-Of-Line
 //
 // Be a happy gopher :-)
 package qqq
